f9767d1e5d56c1c5b26a32273127c052a5b2ae30
sanil.bagzai <sanil.bagzai@rakuten.com>
2021-11-11 21:43:12 +0530

    updating deploy script

--- deployUsingDocker.sh
+++ deployUsingDocker.sh
@@ -1,2 +1,2 @@
-docker run -dit -p 9006:9006 --name=weather sanilbagzai/weather:2.0
-docker run -dit -p 80:80 --link weather:weather sanilbagzai/weather-ui:2.0
+docker run -dit -p 9006:9006 --name=weather sanilbagzai/weather:3.0
+docker run -dit -p 80:80 --link weather:weather sanilbagzai/weather-ui:3.0
